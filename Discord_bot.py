af065c95c2e6f1d25cbc80c1d1d348f8d006c8e5
AartvB <64839449+AartvB@users.noreply.github.com>
2025-06-03 08:38:55 +0200

    Allow for manual messages using the discord bot

--- Discord_bot.py
+++ Discord_bot.py
@@ -16,6 +16,7 @@ class MyClient(discord.Client):
         self.reddit = asyncpraw.Reddit('bot1')
         self.subreddit = await self.reddit.subreddit("dailygames")
         self.bg_task = self.loop.create_task(background_task(self))
+        self.txt_task = self.loop.create_task(process_txt_files(self))
 
 async def doLinkCheck(client):
     print("New post check")
@@ -34,7 +35,7 @@ async def doLinkCheck(client):
                 if guild.name == GUILD:
                     for channel in guild.channels:
                         if channel.name == CHANNEL:
-                            await channel.send('u/{author} has created a new post called "{title}". You can find it here: https://www.reddit.com/r/dailygames/comments/{id}/'.format(author=post.author.name.translate(str.maketrans({'_':  r'\\_', '*':  r'\\*', '~':  r'\\~'})) if post.author else '[deleted]',title=post.title.translate(str.maketrans({'_':  r'\\_', '*':  r'\\*', '~':  r'\\~'})),id=post.id))
+                            await channel.send('u/{author} has created a new post called "{title}". You can find it here: https://www.reddit.com/r/dailygames/comments/{id}/'.format(author=post.author.name.translate(str.maketrans({'_':  r'\_', '*':  r'\*', '~':  r'\~'})) if post.author else '[deleted]',title=post.title.translate(str.maketrans({'_':  r'\_', '*':  r'\*', '~':  r'\~'})),id=post.id))
     conn.close()
 
 async def background_task(client):
@@ -45,6 +46,26 @@ async def background_task(client):
         except Exception as e:
             print(e)
         await asyncio.sleep(60)
+
+async def process_txt_files(client):
+    await client.wait_until_ready()
+    while not client.is_closed():
+        try:
+            filename = 'bot_domain_message.txt'
+            if os.path.isfile(filename):
+                with open(filename, 'r', encoding='utf-8') as f:
+                    content = f.read()
+                print(f"About to send message to #bot-domain:\n{content}")
+                for guild in client.guilds:
+                    for channel in guild.channels:
+                        if channel.name == 'bot-domain':
+                            # Discord messages have a 2000 character limit
+                            for chunk in [content[i:i+2000] for i in range(0, len(content), 2000)]:
+                                await channel.send(chunk)
+                os.remove(filename)
+        except Exception as e:
+            print(f"Error processing bot_domain_message file: {e}")
+        await asyncio.sleep(10)
 
 intents = discord.Intents.default()
 client = MyClient(intents=intents)
